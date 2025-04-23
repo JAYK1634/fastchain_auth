c3588792bebef3fad2bea9d7255ece91c90b8ea4
JAYK1634 <138906045+JAYK1634@users.noreply.github.com>
2025-04-23 12:39:38 +0530

    Update firestore_db.py

--- firestore_db.py
+++ firestore_db.py
@@ -1,7 +1,7 @@
 import requests
 
 def save_user_profile(uid, name, email, token, branch, semester):
-    url = f"https://firestore.googleapis.com/v1/projects/YOUR_PROJECT_ID/databases/(default)/documents/students/{uid}"
+    url = f"https://firestore.googleapis.com/v1/projects/login3-3d1f0/databases/(default)/documents/students/{uid}"
     headers = {"Authorization": f"Bearer {token}"}
     data = {
         "fields": {
@@ -11,10 +11,12 @@ def save_user_profile(uid, name, email, token, branch, semester):
             "semester": {"stringValue": semester}
         }
     }
-    requests.patch(url, headers=headers, json=data)
+    response = requests.patch(url, headers=headers, json=data)
+    return response.json()
+
 
 def get_user_profile(uid, token):
-    url = f"https://firestore.googleapis.com/v1/projects/YOUR_PROJECT_ID/databases/(default)/documents/students/{uid}"
+    url = f"https://firestore.googleapis.com/v1/projects/login3-3d1f0/databases/(default)/documents/students/{uid}"
     headers = {"Authorization": f"Bearer {token}"}
     res = requests.get(url, headers=headers)
     return res.json()
