6c6c8ba5be9dde2034aa7bec5376b18183d9b9db
Prajaktakk <prajaktakumbhare2004@gmail.com>
2025-04-23 12:22:54 +0530

    Update firestore_db.py

--- firestore_db.py
+++ firestore_db.py
@@ -1,10 +1,10 @@
 import requests
 import os
 
-PROJECT_ID = "<fastfoodchain-8a5cb>"
+PROJECT_ID = "fastfoodchain-8a5cb" 
 
 def save_user_profile(uid, name, email, id_token):
-    url = f"https://firestore.googleapis.com/v1/projects/{fastfoodchain-8a5cb}/databases/(default)/documents/users/{uid}"
+    url = f"https://firestore.googleapis.com/v1/projects/{PROJECT_ID}/databases/(default)/documents/users/{uid}"  # Fixed
     headers = {"Authorization": f"Bearer {id_token}"}
     data = {
         "fields": {
@@ -15,6 +15,6 @@ def save_user_profile(uid, name, email, id_token):
     return requests.patch(url, json=data, headers=headers).json()
 
 def get_user_profile(uid, id_token):
-    url = f"https://firestore.googleapis.com/v1/projects/{fastfoodchain-8a5cb}/databases/(default)/documents/users/{uid}"
+    url = f"https://firestore.googleapis.com/v1/projects/{PROJECT_ID}/databases/(default)/documents/users/{uid}"  # Fixed
     headers = {"Authorization": f"Bearer {id_token}"}
     return requests.get(url, headers=headers).json()
