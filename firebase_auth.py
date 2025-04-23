d552b3070450ec86c93cc20f2cfdd6dda4e9f598
JAYK1634 <138906045+JAYK1634@users.noreply.github.com>
2025-04-23 12:49:58 +0530

    Update firebase_auth.py

--- firebase_auth.py
+++ firebase_auth.py
@@ -1,23 +1,32 @@
-import os
 import requests
 
-API_KEY = os.environ.get("
-AIzaSyBdst7Cj28WmPpMeYSYPVsNFnidDce1R3k")
+API_KEY = "AIzaSyBdst7Cj28WmPpMeYSYPVsNFnidDce1R3k"  # your Firebase Web API key
 
 def signup_user(email, password):
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={API_KEY}"
-    payload = {"email": email, "password": password, "returnSecureToken": True}
+    payload = {
+        "email": email,
+        "password": password,
+        "returnSecureToken": True
+    }
     res = requests.post(url, json=payload)
     return res.json()
 
 def login_user(email, password):
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={API_KEY}"
-    payload = {"email": email, "password": password, "returnSecureToken": True}
+    payload = {
+        "email": email,
+        "password": password,
+        "returnSecureToken": True
+    }
     res = requests.post(url, json=payload)
     return res.json()
 
 def reset_password(email):
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key={API_KEY}"
-    payload = {"requestType": "PASSWORD_RESET", "email": email}
+    payload = {
+        "requestType": "PASSWORD_RESET",
+        "email": email
+    }
     res = requests.post(url, json=payload)
     return res.json()
